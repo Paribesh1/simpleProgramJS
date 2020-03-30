f0a8ec8242cd42583194d381736a3f65e1f34b02
paribesh.koirala1 <paribesh.koirala1@gmail.com>
2020-03-30 20:16:59 +0545

    duplicateElementInArray

---
+++ pickingLetterOfString.js
@@ -0,0 +1,19 @@
+var firstName = "Paribesh";
+    var firstNameFirstLetter = firstName[0];
+    var firstNameLastLetter = firstName[firstName.length - 1];
+
+        console.log(firstNameFirstLetter);
+        console.log(firstNameLastLetter);
+
+
+var lastName = "Koirala";
+    var lastNameSecondLetter = lastName[1];
+    var lastNameThirdLetter = lastName[2];
+        console.log(lastNameSecondLetter);
+        console.log(lastNameThirdLetter);
+        
+
+
+        
+
+
